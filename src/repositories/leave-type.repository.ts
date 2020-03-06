5efd466576d86a006940a766b14c306ace59cf28
Akshay Kumar <email.akshay98@gmail.com>
2020-03-06 15:12:12 +0530

    add leaveType repository

---
+++ src/repositories/leave-type.repository.ts
@@ -0,0 +1,16 @@
+import {DefaultCrudRepository} from '@loopback/repository';
+import {LeaveType, LeaveTypeRelations} from '../models';
+import {DbDataSource} from '../datasources';
+import {inject} from '@loopback/core';
+
+export class LeaveTypeRepository extends DefaultCrudRepository<
+  LeaveType,
+  typeof LeaveType.prototype.type,
+  LeaveTypeRelations
+> {
+  constructor(
+    @inject('datasources.db') dataSource: DbDataSource,
+  ) {
+    super(LeaveType, dataSource);
+  }
+}
